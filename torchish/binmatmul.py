5517250d0ee850c81dcb1b46948c871014958f6f
Ryan Goetz <ryan.m.goetz@gmail.com>
2025-04-29 23:51:02 -0400

    typo

--- torchish/binmatmul.py
+++ torchish/binmatmul.py
@@ -10,7 +10,7 @@ def bitpack_2d(input: torch.Tensor, kernel: int = 0) -> torch.Tensor:
     zero padding up to the nearest multiple of 8.
 
     Args:
-        input: A [N, M] boolean tensor.
+        input: A [N, M] boolean tensor (or uint8 tensor of 0s and 1s).
 
     Returns:
         torch.Tensor: A [N, K] tensor with dtype uint8.
@@ -18,7 +18,7 @@ def bitpack_2d(input: torch.Tensor, kernel: int = 0) -> torch.Tensor:
     return torch.ops.torchish.bitpack_2d(input, kernel) # [K, M]  
 
 def bitunpack_2d(packed: torch.Tensor, N: int, M: int, dtype: torch.dtype = torch.bool, kernel: int = 0) -> torch.Tensor:
-    """Unpacks a @D bit-packed bool-like tensor of uint8 into a proper boolean tensor.
+    """Unpacks a 2D bit-packed bool-like tensor of uint8 into a proper boolean tensor.
 
     Args:
         packed: A 2D bitpacked tensor of dtype uint8.
